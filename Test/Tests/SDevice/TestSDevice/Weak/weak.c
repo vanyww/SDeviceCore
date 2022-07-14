8adf9b027bb5aaaf4f05d78519302eafdde36878
Ivan <vanyww@mail.ru>
2022-07-14 23:18:31 +0300

    Update tests to use new error processing

--- Test/Tests/SDevice/TestSDevice/Weak/weak.c
+++ Test/Tests/SDevice/TestSDevice/Weak/weak.c
@@ -1,17 +1,22 @@
-#include "SDeviceCore/assert.h"
+#include "SDeviceCore/errors.h"
 #include "SDeviceCore/heap.h"
 
 #include <stdlib.h>
 #include <stdbool.h>
 
-void SDeviceAssertFailed(char *file, int line)
+void _SDeviceProcessAssertFail(char *file, int line)
 {
    for(;;) { }
 }
 
-void SDeviceProcessRuntimeError(void *_handle)
+void _SDeviceProcessUnhandledThrow(const void *_handle)
 {
    SDeviceAssert(false);
+}
+
+void _SDeviceLogStatus(const void *_handle)
+{
+
 }
 
 void * SDeviceMalloc(size_t size)
