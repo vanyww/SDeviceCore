a8b45332d5d2d3eb1ac0c923166d6b6af8acf8ce
Ivan <vanyww@mail.ru>
2022-07-22 20:59:52 +0300

    Increment version

--- Inc/SDeviceCore/core.h
+++ Inc/SDeviceCore/core.h
@@ -7,7 +7,7 @@
 
 #define __SDEVICE_CORE_VERSION_MAJOR 2
 #define __SDEVICE_CORE_VERSION_MINOR 1
-#define __SDEVICE_CORE_VERSION_PATCH 0
+#define __SDEVICE_CORE_VERSION_PATCH 1
 
 /* handle *************************************************************************************************************/
 
