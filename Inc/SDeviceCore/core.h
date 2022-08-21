456c07eb6d559ad1d2a74bcc510acfe87dfbed20
Ivan <vanyww@mail.ru>
2022-08-21 14:16:07 +0300

    Fix formatting

--- Inc/SDeviceCore/core.h
+++ Inc/SDeviceCore/core.h
@@ -5,12 +5,11 @@
 #include <stdint.h>
 #include <stddef.h>
 
-/* version *************************************************************************************************************/
+/* version ************************************************************************************************************/
 
 #define __SDEVICE_CORE_VERSION_MAJOR 2
 #define __SDEVICE_CORE_VERSION_MINOR 2
 #define __SDEVICE_CORE_VERSION_PATCH 0
-
 #define __SDEVICE_CORE_VERSION ((SDeviceVersion)                                                                       \
 {                                                                                                                      \
    .Major = __SDEVICE_CORE_VERSION_MAJOR,                                                                              \
