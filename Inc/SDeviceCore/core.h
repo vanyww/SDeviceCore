790f4c65c79678c09d196e1a27bcb78807c3d965
Ivan <vanyww@mail.ru>
2022-08-27 23:03:14 +0300

    Update version to 2.3.0

--- Inc/SDeviceCore/core.h
+++ Inc/SDeviceCore/core.h
@@ -8,8 +8,8 @@
 /* version ************************************************************************************************************/
 
 #define __SDEVICE_CORE_VERSION_MAJOR 2
-#define __SDEVICE_CORE_VERSION_MINOR 2
-#define __SDEVICE_CORE_VERSION_PATCH 1
+#define __SDEVICE_CORE_VERSION_MINOR 3
+#define __SDEVICE_CORE_VERSION_PATCH 0
 #define __SDEVICE_CORE_VERSION ((SDeviceVersion)                                                                       \
 {                                                                                                                      \
    .Major = __SDEVICE_CORE_VERSION_MAJOR,                                                                              \
