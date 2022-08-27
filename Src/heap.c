82304f064390e884b8e279843c31cd2f3147a7b3
Ivan <vanyww@mail.ru>
2022-08-27 23:02:33 +0300

    Add NULL check to SDeviceMalloc

--- Src/heap.c
+++ Src/heap.c
@@ -1,10 +1,17 @@
 #include "SDeviceCore/heap.h"
+#include "SDeviceCore/errors.h"
+#include "SDeviceCore/global.h"
 
 #include <stdlib.h>
 
 __attribute__((weak)) void * SDeviceMalloc(size_t size)
 {
-   return malloc(size);
+   void *memory = malloc(size);
+
+   if(memory == NULL)
+      SDeviceThrow(CoreGlobalSDeviceHandle, CORE_GLOBAL_SDEVICE_EXCEPTION_OUT_OF_MEMORY);
+
+   return memory;
 }
 
 __attribute__((weak)) void SDeviceFree(void *_handle)
