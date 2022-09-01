c1db29684ad9f0c15db71f5087243e4b3af1bd85
Ivan <vanyww@mail.ru>
2022-09-01 14:23:54 +0300

    Update global sdevice to use new string name macros

--- Src/global.c
+++ Src/global.c
@@ -1,19 +1,19 @@
 #include "SDeviceCore/global.h"
 
-static const char DeviceName[] = "CoreGlobal\0";
-
 __SDEVICE_RUNTIME_DATA_FORWARD_DECLARATION(CoreGlobal);
+
+__SDEVICE_STRING_NAME_DEFINITION(CoreGlobal);
 
 struct __SDEVICE_RUNTIME_DATA(CoreGlobal) { };
 
-__SDEVICE_HANDLE_DEFINITION(CoreGlobal);
+__SDEVICE_HANDLE_DECLARATION(CoreGlobal);
 
 __SDEVICE_HANDLE(CoreGlobal) *CoreGlobalSDeviceHandle = &(__SDEVICE_HANDLE(CoreGlobal))
 {
    .Header =
    {
       .Context = NULL,
-      .NameNode = { .Name = DeviceName, .OuterNode = NULL },
+      .NameNode = { .Name = __SDEVICE_STRING_NAME(CoreGlobal), .OuterNode = NULL },
       .LatestStatus = CORE_GLOBAL_SDEVICE_STATUS_OK
    }
 };
