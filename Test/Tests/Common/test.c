87521f01b3d7499919cec6c7071a20233c42a539
Ivan Shestakov <vanyww@mail.ru>
2022-11-22 03:57:37 +0300

    Update tests

--- Test/Tests/Common/test.c
+++ Test/Tests/Common/test.c
@@ -2,6 +2,8 @@
 
 #include "SDeviceCore/common.h"
 
+#include <stdint.h>
+
 bool TestCommonMaxMacro(void)
 {
    int min = -10, max = 10;
@@ -28,5 +30,20 @@ bool TestCommonLengthOf(void)
    int TestArray[TestArrayLength];
 
    return __LENGTHOF(TestArray) == TestArrayLength;
+}
+
+bool TestBitSizeof(void)
+{
+   uint32_t TestValue;
+
+   return __BIT_SIZEOF(TestValue) == 4 * CHAR_BIT;
+}
+
+bool TestCommonBitSizeOfMember(void)
+{
+   typedef int TestType;
+   struct TestStructure { TestType TestValue; };
+
+   return __BIT_SIZEOF_MEMBER(struct TestStructure, TestValue) == sizeof(TestType) * CHAR_BIT;
 }
 
