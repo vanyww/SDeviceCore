0cf525e03bac89d3d240ed896fa1f60c2622c2ce
Ivan Shestakov <vanyww@mail.ru>
2022-11-25 05:18:33 +0300

    Implement __SDEVICE_USE_SIMPLE_ASSERT flag to allow lower memory consumption Flag makes assert fail callback accept zero arguments (instead of filename and line)

--- Src/errors.c
+++ Src/errors.c
@@ -1,10 +1,16 @@
 #include "SDeviceCore/errors.h"
 #include "SDeviceCore/common.h"
 
+#ifdef __SDEVICE_USE_SIMPLE_ASSERT
+__attribute__((weak)) void _SDeviceProcessAssertFail(void)
+#else
 __attribute__((weak)) void _SDeviceProcessAssertFail(char *file, int line)
+#endif
 {
+#ifndef __SDEVICE_USE_SIMPLE_ASSERT
    __UNUSED_PARAMETER(file);
    __UNUSED_PARAMETER(line);
+#endif
 
    for(;;) { }
 }
