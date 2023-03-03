2226bf8fe394a2e2b5cb84be6cca40ea0f55959c
Ivan Shestakov <vanyww@mail.ru>
2023-03-03 05:44:27 +0300

    Reorder some errors.h declarations for better readability

--- Inc/SDeviceCore/errors.h
+++ Inc/SDeviceCore/errors.h
@@ -2,8 +2,6 @@
 
 #include <stddef.h>
 
-/* cexception *********************************************************************************************************/
-
 typedef const void * CExceptionType;
 
 #ifdef SDEVICE_USE_EXTERNAL_ERROR_CONFIG
@@ -14,21 +12,13 @@ typedef const void * CExceptionType;
 #define CEXCEPTION_T CExceptionType
 #define CEXCEPTION_NO_CATCH_HANDLER(id) SDeviceProcessUnhandledThrow(id)
 
-#ifdef SDEVICE_USE_EXTERNAL_ERROR_CONFIG
-#include "sdevice_error_config.h"
-#endif
-
 #include "../../Submodules/cexception/lib/CException.h"
-
-/* assertations *******************************************************************************************************/
 
 #ifdef SDEVICE_USE_ASSERT
 #ifdef SDEVICE_USE_SIMPLE_ASSERT
 #define SDeviceAssert(expression) ((expression) ? (void)0U : SDeviceProcessAssertFail())
-void SDeviceProcessAssertFail(void);
 #else
 #define SDeviceAssert(expression) ((expression) ? (void)0U : SDeviceProcessAssertFail(__FILE__, __LINE__))
-void SDeviceProcessAssertFail(char *file, int line);
 #endif
 #define SDeviceEvalAssert(expression) SDeviceAssert(expression)
 #else
@@ -42,15 +32,12 @@ void SDeviceProcessAssertFail(char *file, int line);
 void SDeviceProcessAssertFail(void);
 #else
 #define SDeviceDebugAssert(expression) ((expression) ? (void)0U : SDeviceProcessAssertFail(__FILE__, __LINE__))
-void SDeviceProcessAssertFail(char *file, int line);
 #endif
 #define SDeviceDebugEvalAssert(expression) SDeviceAssert(expression)
 #else
 #define SDeviceDebugAssert(expression) ((void)0U)
 #define SDeviceDebugEvalAssert(expression) eSDeviceProcessLogStatusxpression
 #endif
-
-/* logging ************************************************************************************************************/
 
 #ifdef SDEVICE_USE_STATUS_LOG
 #define SDeviceLogStatus(handle, status) (                                                                             \
@@ -59,19 +46,22 @@ void SDeviceProcessAssertFail(char *file, int line);
    $handle->Header.LatestStatus = (status);                                                                            \
    SDeviceProcessLogStatus($handle);                                                                                   \
 })
-void SDeviceProcessLogStatus(const void *_handle);
+
 #else
 #define SDeviceLogStatus(handle, status) ((SDeviceCommonHandle *)(handle))->Header.LatestStatus = (status)
 #endif
-
-/* throw **************************************************************************************************************/
-
 #define SDeviceThrow(handle, exception) (                                                                              \
 {                                                                                                                      \
    SDeviceCommonHandle *$handle = (SDeviceCommonHandle *)(handle);                                                     \
    $handle->Header.LatestStatus = (exception);                                                                         \
    Throw($handle);                                                                                                     \
 })
+
+#ifdef SDEVICE_USE_SIMPLE_ASSERT
+void SDeviceProcessAssertFail(void);
+#else
+void SDeviceProcessAssertFail(char *file, int line);
+#endif
+void SDeviceProcessLogStatus(const void *_handle);
 void SDeviceProcessUnhandledThrow(CEXCEPTION_T _handle);
 
-/**********************************************************************************************************************/
