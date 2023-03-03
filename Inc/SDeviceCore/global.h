0f59b5e3e95c557a90ce0a846fb1b64ac02bb79a
Ivan Shestakov <vanyww@mail.ru>
2023-03-03 05:41:15 +0300

    Move global sdevice's init data declarations to private interface

--- Inc/SDeviceCore/global.h
+++ Inc/SDeviceCore/global.h
@@ -3,7 +3,6 @@
 #include "SDeviceCore/core.h"
 
 SDEVICE_HANDLE_FORWARD_DECLARATION(CoreGlobal);
-SDEVICE_INIT_DATA_FORWARD_DECLARATION(CoreGlobal);
 
 typedef enum
 {
@@ -14,9 +13,5 @@ typedef enum
 {
    CORE_GLOBAL_SDEVICE_EXCEPTION_OUT_OF_MEMORY
 } CoreGlobalSDeviceException;
-
-struct SDEVICE_INIT_DATA(CoreGlobal) { };
-
-SDEVICE_STRING_NAME_DECLARATION(CoreGlobal);
 
 extern SDEVICE_HANDLE(CoreGlobal) *CoreGlobalSDeviceHandle;
