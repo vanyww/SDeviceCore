3ee264d210605f868eec5ed2b983e96c45ea488d
Avvesey Erockhov <kolyann088@gmail.com>
2023-09-09 13:22:00 +0300

    Implement SDevice panic test

--- Test/Tests/Weak/TestWeak.c
+++ Test/Tests/Weak/TestWeak.c
@@ -20,7 +20,23 @@ TEST(Weak, SDeviceAssert)
    TEST_ASSERT(AssertFailedCalled);
 }
 
+TEST(Weak, SDevicePanic)
+{
+   void *context = NULL;
+   void *owner = NULL;
+   SDeviceHandleIdentifier id = 0;
 
+   SDEVICE_INIT_DATA(TestDevice) init = { .testDeviceData ={ .FirstValue = 0, .SecondValue = 0 } };
+
+   _cleanup SDEVICE_HANDLE(TestDevice) *handle = SDEVICE_CREATE_HANDLE(TestDevice)(&init, owner, id, context);
+
+   int16_t status = TEST_DEVICE_SDEVICE_SOME_PANIC;
+
+   SDevicePanic(handle, TEST_DEVICE_SDEVICE_SOME_PANIC);
+
+   TEST_ASSERT_EQUAL(handle, LastThrowedPanicHandle);
+   TEST_ASSERT_EQUAL(status, SDeviceGetHandleLatestStatus(LastThrowedPanicHandle));
+}
 
 TEST(Weak, SDeviceLogStatus)
 {
@@ -42,5 +58,6 @@ TEST(Weak, SDeviceLogStatus)
 TEST_GROUP_RUNNER(Weak)
 {
    RUN_TEST_CASE(Weak, SDeviceAssert);
+   RUN_TEST_CASE(Weak, SDevicePanic);
    RUN_TEST_CASE(Weak, SDeviceLogStatus);
 }
