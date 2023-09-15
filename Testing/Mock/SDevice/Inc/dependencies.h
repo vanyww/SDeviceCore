1390371d94dede9f4c4085d3ee6c2baa0117911c
Ivan Shestakov <vanyww@mail.ru>
2023-09-16 02:08:09 +0300

    Update testing mock SDevice dependencies checks

--- Testing/Mock/SDevice/Inc/dependencies.h
+++ Testing/Mock/SDevice/Inc/dependencies.h
@@ -2,6 +2,6 @@
 
 #include "SDeviceCore/core.h"
 
-#if (SDEVICE_CORE_VERSION_MAJOR != 8) || (SDEVICE_CORE_VERSION_MINOR < 0)
+#if (SDEVICE_CORE_VERSION_MAJOR != 9) || (SDEVICE_CORE_VERSION_MINOR < 0)
    #error SDevice core version is incorrect.
 #endif
